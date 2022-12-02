98ac07193d9b503db0a29a7f3414da3136c82c4e
Tom Gagnier (tgagnier) <tgagnier@cisco.com>
2022-12-02 02:15:18 -0500

    day 2 refactor

--- src/main/kotlin/dec02/main.kt
+++ src/main/kotlin/dec02/main.kt
@@ -1,25 +1,28 @@
 package dec02
 
+import java.io.File
+
 fun main() {
+    fun printScore(strategyGuide: String, scoring: Map<String, Int>) =
+        println(File(strategyGuide).readLines().sumOf { match -> scoring[match]!! })
+
     fun printAnswers(path: String) {
         println(path)
-        val guide = java.io.File(path).readLines()
-        println(guide.sumOf { match ->
-            mapOf(
+        printScore(
+            strategyGuide = path, scoring = mapOf(
                 "B X" to 1, "C Y" to 2, "A Z" to 3,
                 "A X" to 4, "B Y" to 5, "C Z" to 6,
                 "C X" to 7, "A Y" to 8, "B Z" to 9,
-            )[match]!!
-        })
-        println(guide.sumOf { map ->
-            mapOf(
+            )
+        )
+        printScore(
+            strategyGuide = path, scoring = mapOf(
                 "B X" to 1, "C X" to 2, "A X" to 3,
                 "A Y" to 4, "B Y" to 5, "C Y" to 6,
                 "C Z" to 7, "A Z" to 8, "B Z" to 9
-            )[map]!!
-        })
+            )
+        )
     }
-
     printAnswers("src/main/kotlin/dec02/example")
     printAnswers("src/main/kotlin/dec02/puzzle-input")
 }
