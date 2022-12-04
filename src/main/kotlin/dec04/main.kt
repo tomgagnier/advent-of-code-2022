360c374c17f13c5d8c845fc631674840cdee74f0
Tom Gagnier (tgagnier) <tgagnier@cisco.com>
2022-12-04 01:26:08 -0500

    Day 4 solution

---
+++ src/main/kotlin/dec04/main.kt
@@ -0,0 +1,23 @@
+package dec04
+
+fun main() {
+    listOf("example", "puzzle-input")
+        .forEach { filename ->
+            val input = java.io.File("src/main/kotlin/dec04/$filename")
+                .readLines()
+                .map { it.split(Regex("[,-]")).map { s -> s.toInt() } }
+            println(filename)
+            println("1) ${input.count { inside(it) }}")
+            println("2) ${input.count { duplicated(it) }}")
+        }
+}
+
+fun inside(a: List<Int>) =
+    a[0] <= a[2] && a[3] <= a[1] ||
+    a[2] <= a[0] && a[1] <= a[3]
+
+fun duplicated(a: List<Int>) =
+    a[0] <= a[2] && a[2] <= a[1] ||
+    a[0] <= a[3] && a[2] <= a[1] ||
+    a[2] <= a[0] && a[0] <= a[3] ||
+    a[2] <= a[1] && a[1] <= a[3]
