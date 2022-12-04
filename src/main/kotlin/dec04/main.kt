ba605295c3d6b57a0a59284157d08e42c6b4771b
Tom Gagnier (tgagnier) <tgagnier@cisco.com>
2022-12-04 09:27:07 -0500

    Refactor day 4 solution

--- src/main/kotlin/dec04/main.kt
+++ src/main/kotlin/dec04/main.kt
@@ -1,6 +1,16 @@
 package dec04
 
 fun main() {
+    fun inside(a: List<Int>) =
+        a[0] <= a[2] && a[3] <= a[1] ||
+        a[2] <= a[0] && a[1] <= a[3]
+
+    fun duplicated(a: List<Int>) =
+        a[0] <= a[2] && a[2] <= a[1] ||
+        a[0] <= a[3] && a[2] <= a[1] ||
+        a[2] <= a[0] && a[0] <= a[3] ||
+        a[2] <= a[1] && a[1] <= a[3]
+
     listOf("example", "puzzle-input")
         .forEach { filename ->
             val input = java.io.File("src/main/kotlin/dec04/$filename")
@@ -11,13 +21,3 @@ fun main() {
             println("2) ${input.count { duplicated(it) }}")
         }
 }
-
-fun inside(a: List<Int>) =
-    a[0] <= a[2] && a[3] <= a[1] ||
-    a[2] <= a[0] && a[1] <= a[3]
-
-fun duplicated(a: List<Int>) =
-    a[0] <= a[2] && a[2] <= a[1] ||
-    a[0] <= a[3] && a[2] <= a[1] ||
-    a[2] <= a[0] && a[0] <= a[3] ||
-    a[2] <= a[1] && a[1] <= a[3]
